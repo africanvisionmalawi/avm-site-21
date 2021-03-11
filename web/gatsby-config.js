361bf2c764eac67fb26ed152f990a4cabf35a31d
Nigel Bunner <nigelbunner@gmail.com>
2021-03-11 21:44:34 +0000

    Adds the read token

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -112,7 +112,7 @@ module.exports = {
       options: {
         projectId: process.env.GATSBY_SANITY_PROJECT_ID,
         dataset: process.env.GATSBY_SANITY_DATASET,
-        token: process.env.SANITY_READ_TOKEN,
+        token: process.env.GATSBY_SANITY_READ_TOKEN,
         watchMode: true,
         overlayDrafts: true,
       },
