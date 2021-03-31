a85496108ad5147d361520f1355543e419683edc
Nigel Bunner <nigelbunner@gmail.com>
2021-03-31 22:32:39 +0100

    Thank yous page sort of working but with missing data??

--- web/src/components/pageLinks/PageLinks.js
+++ web/src/components/pageLinks/PageLinks.js
@@ -9,6 +9,40 @@ const Heading = styled.h2`
   text-align: center;
 `;
 
+const PageLinksWithPhotos = ({ pageLinks }) => {
+  return (
+    <>
+      {pageLinks.length
+        ? pageLinks.map((pageLink) => {
+            return (
+              <React.Fragment key={pageLink.id}>
+                <CardSingle
+                  // smallImage={pageLink.smallImage}
+                  url={
+                    pageLink.url
+                      ? getPath(
+                          pageLink.url.category.slug.current,
+                          pageLink.url.slug.current
+                        )
+                      : null
+                  }
+                  extUrl={pageLink.extUrl ? pageLink.extUrl : null}
+                  title={pageLink.linkTitle ? pageLink.linkTitle : null}
+                  linkText={pageLink.linkText ? pageLink.linkText : null}
+                  fixed={pageLink.photo ? pageLink.photo.asset.fixed : null}
+                  photo={pageLink.photo ? pageLink.photo : null}
+                  featured={pageLink.featured ? pageLink.featured : false}
+                  // showPageLink={pageLink.showPageLink}
+                  // hideOtherPhotos={pageLink.hideOtherPhotos}
+                />
+              </React.Fragment>
+            );
+          })
+        : null}
+    </>
+  );
+};
+
 export const PageLinks = ({
   pageLinks,
   displayHeading,
@@ -17,43 +51,25 @@ export const PageLinks = ({
   hideOtherPhotos,
 }) => {
   // console.log("pageLink photo ", pageLinks[0].photo.asset);
+  console.log("pageLinks ", pageLinks);
   const featuredLinks = getFeaturedLinks(pageLinks, true);
   const otherLinks = getFeaturedLinks(pageLinks, false);
-  console.log("featuredLinks ", featuredLinks);
-  console.log("otherLinks", otherLinks);
+  // console.log("featuredLinks ", featuredLinks);
+  // console.log("otherLinks", otherLinks);
 
   return (
     <div>
       {displayHeading ? <Heading>{heading}</Heading> : null}
-      <div className={styles.cardContWide}>
-        {pageLinks.length
-          ? pageLinks.map((pageLink) => {
-              return (
-                <React.Fragment key={pageLink.id}>
-                  <CardSingle
-                    // smallImage={pageLink.smallImage}
-                    url={
-                      pageLink.url
-                        ? getPath(
-                            pageLink.url.category.slug.current,
-                            pageLink.url.slug.current
-                          )
-                        : null
-                    }
-                    extUrl={pageLink.extUrl ? pageLink.extUrl : null}
-                    title={pageLink.linkTitle ? pageLink.linkTitle : null}
-                    linkText={pageLink.linkText ? pageLink.linkText : null}
-                    fixed={pageLink.photo ? pageLink.photo.asset.fixed : null}
-                    photo={pageLink.photo ? pageLink.photo : null}
-                    featured={pageLink.featured ? pageLink.featured : false}
-                    // showPageLink={pageLink.showPageLink}
-                    // hideOtherPhotos={pageLink.hideOtherPhotos}
-                  />
-                </React.Fragment>
-              );
-            })
-          : null}
-      </div>
+      {featuredLinks.length ? (
+        <div className={styles.cardContWide}>
+          <PageLinksWithPhotos pageLinks={featuredLinks} />
+        </div>
+      ) : null}
+      {otherLinks.length ? (
+        <div className={styles.cardCont}>
+          <PageLinksWithPhotos pageLinks={otherLinks} />
+        </div>
+      ) : null}
     </div>
   );
 };
