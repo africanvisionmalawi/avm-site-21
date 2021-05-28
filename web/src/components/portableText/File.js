8938150dc88fc0d1061ca73507958d97aa427281
Nigel Bunner <nigelbunner@gmail.com>
2021-05-28 22:13:28 +0100

    Displays file uploads as links to download

---
+++ web/src/components/portableText/File.js
@@ -0,0 +1,44 @@
+import { styled } from "linaria/react";
+import React from "react";
+import { IconContext } from "react-icons";
+import { ImFilePdf } from "react-icons/im";
+
+const Container = styled.a`
+  align-items: center;
+  border: 1px dashed #f99d1c;
+  display: flex;
+  margin: 1rem auto;
+  padding: 2rem;
+  width: 80%;
+  svg {
+    height: 80px;
+    margin-right: 1rem;
+  }
+`;
+
+const Heading = styled.h4`
+  margin-bottom: 0;
+`;
+
+const Text = styled.p`
+  font-style: italic;
+  margin-bottom: 0;
+`;
+
+export default ({ node }) => {
+  if (!node || !node.asset || !node.asset._id) {
+    return null;
+  }
+
+  return (
+    <Container href={node.asset.url}>
+      <IconContext.Provider value={{ size: "3rem" }}>
+        <ImFilePdf />
+      </IconContext.Provider>
+      <div>
+        {node.description ? <Heading>{node.description}</Heading> : null}
+        <Text>{node.asset.originalFilename}</Text>
+      </div>
+    </Container>
+  );
+};
--- web/src/components/portableText/serializers.js
+++ web/src/components/portableText/serializers.js
@@ -1,11 +1,13 @@
 import { Video } from "components/videos";
 import React from "react";
 import Figure from "./Figure";
+import File from "./File";
 
 const serializers = {
   types: {
     mainImage: Figure,
     video: ({ node }) => <Video url={node.url} text={node.text} />,
+    pdf: File,
   },
 };
 
