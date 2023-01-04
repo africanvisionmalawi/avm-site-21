828ce393277f5650e03e027e76a76a4effcc0b20
Nigel Bunner <nigelbunner@gmail.com>
2023-01-04 21:47:42 +0000

    Adds image

--- web/pages/[slug].js
+++ web/pages/[slug].js
@@ -1,13 +1,43 @@
+import groq from 'groq'
+import imageUrlBuilder from '@sanity/image-url'
 import client from '/client'
+
+function urlFor (source) {
+  return imageUrlBuilder(client).image(source)
+}
 
 const Page = ({data}) => {
   
   return (
     <article>
-      <h1>{data?.slug?.current}</h1>
+      <h1>{data?.title}</h1>
+      {data?.hero && (
+        <div>
+          <img
+            src={urlFor(data.hero.image)
+              .width(100)
+              .url()}
+          />
+        </div>
+      )}
+
     </article>
   )
 }
+
+const query = groq`*[_type == "page" && slug.current == $slug][0]{ 
+  slug, 
+  id,
+  title,
+  description,
+  indexPage,
+  pageHeading, 
+  "categoryTitle": category->title, 
+  "categorySlug": category->slug.current,
+  hero, 
+  bannerMsg,
+}`
+
 
 export async function getStaticPaths() {
   const paths = await client.fetch(
@@ -23,10 +53,9 @@ export async function getStaticPaths() {
 export async function getStaticProps(context) {
   // It's important to default the slug so that it doesn't return "undefined"
   const { slug = "" } = context.params
-  const data = await client.fetch(`
-    *[_type == "page" && slug.current == $slug][0]    
-  `, { slug });
-  console.log("slug ", slug, data);
+  const data = await client.fetch(query, { slug })  
+  console.log("hero ", data.hero.mobileImage);
+  console.log("slug ", slug, data);  
   return {
     props: {
       data
